bbf4eb46ee704b8b6b0eb896050dc7f71d068aeb
Amber Jones <amberlovescats14@gmail.com>
2019-09-03 20:25:44 -0500

    if statments and while

--- src/com/amber/Main.java
+++ src/com/amber/Main.java
@@ -1,9 +1,27 @@
 package com.amber;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please Choose a Number: ");
+        int num = scan.nextInt();
+
+        if(num % 5 == 0) System.out.println("fizz");
+        if(num % 3 == 0) System.out.println("buzz");
+
+
+        //! WHILE
+        Scanner scanText = new Scanner(System.in);
+        String input = "";
+        while(!input.equals("quit")){
+            System.out.print("Input: ");
+            input = scanText.next().toLowerCase();
+            System.out.println(input);
+        }
+
     }
 }
 
