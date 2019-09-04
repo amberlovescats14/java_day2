a97331487b975d627b9d87b6a92ac6d698c3ed36
Amber Jones <amberlovescats14@gmail.com>
2019-09-04 08:44:27 -0500

    refactored

--- src/com/amber/Main.java
+++ src/com/amber/Main.java
@@ -43,7 +43,7 @@ public class Main {
         Scanner principalIn = new Scanner(System.in);
         double principalSave;
         while(true) {
-            System.out.println("Principal Borrowing Amount; 1K -1M: " );
+            System.out.print("Principal Borrowing Amount; 1K -1M: " );
             principalSave = principalIn.nextDouble();
             if(principalSave >= 1000 && principalSave < 1_000_000) break;
             System.out.println("Enter a value between 1K and 1M");
@@ -52,33 +52,33 @@ public class Main {
 
 
         Scanner annualIn = new Scanner(System.in);
-        double annualSave;
+        double monthlyInterest;
         while(true){
-            System.out.println("Annual Interest Rate: ");
-            annualSave = (annualIn.nextDouble() / PERCENT) / MONTHS_IN_YEAR;
-            if(annualSave >= 0 && annualSave < 10) break;
+            System.out.print("Annual Interest Rate: ");
+            monthlyInterest = (annualIn.nextDouble() / PERCENT) / MONTHS_IN_YEAR;
+            if(monthlyInterest >= 0 && monthlyInterest < 10) break;
             System.out.println("Please enter a number between .1-10");
         }
 
 
-        Scanner periodIn = new Scanner(System.in);
-        int periodSave;
+        Scanner years = new Scanner(System.in);
+        int loanMonths;
         while(true){
-            System.out.println("How many year would you like to finance: ");
-            periodSave = periodIn.nextInt();
-            if( periodSave >= 10 && periodSave <= 50) break;
+            System.out.print("How many year would you like to finance: ");
+            loanMonths = years.nextInt();
+            if( loanMonths >= 10 && loanMonths <= 50) break;
             System.out.println("Please choose a number between 10-50");
         }
 
+//        double mortgage = principal
+//                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+//                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
-        double top = annualSave * Math.pow((1 + annualSave), periodSave);
-        System.out.println("top: " + top);
-        double bottom = Math.pow((1 + annualSave), periodSave) - 1;
-        System.out.println("bottom : " + bottom);
+
+        double top = (monthlyInterest * Math.pow(1 + monthlyInterest, loanMonths));
+        double bottom = (Math.pow(1 + monthlyInterest, loanMonths) - 1);
         double divided = top / bottom;
-        System.out.println("divided: " + divided);
         double multiply = principalSave * divided;
-        System.out.println("multiple : " + multiply);
 
         String mortgage = NumberFormat.getCurrencyInstance().format(multiply);
         System.out.println(mortgage);
